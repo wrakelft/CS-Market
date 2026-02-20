e9b893d67c888a45e78cc4cb7fa469bd3e154e50
kkkeri <kudiyash2005@mail.ru>
2026-02-20 06:01:32 +0300

    up

--- backend/src/main/java/ru/itmo/backend/jobs/ReservationCleanupScheduler.java
+++ backend/src/main/java/ru/itmo/backend/jobs/ReservationCleanupScheduler.java
@@ -1,4 +1,25 @@
 package ru.itmo.backend.jobs;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import ru.itmo.backend.dao.CleanupDao;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
 public class ReservationCleanupScheduler {
+
+    private final CleanupDao dao;
+
+    @Scheduled(fixedDelayString = "${app.reservations.cleanupDelayMs:30000}")
+    @Transactional
+    public void run() {
+        int cleared = dao.cleanupExpiredReservations();
+        if (cleared > 0) {
+            log.info("cleanup_expired_reservations cleared {}", cleared);
+        }
+    }
 }
